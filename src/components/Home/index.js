d51f75a03f91d28b1d5f558ebae70b247a732b57
SofiMesh <sofimeshvildishvili@gmail.com>
2023-09-24 14:28:54 -0400

    home page added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,25 @@
-const Home = () => ('Hello')
+import LogoTitle from '../assets/images/logo-s.png';
+
+
+
+const Home = () => {
+
+    return (
+        <div claasName="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm
+                <img src={LogoTitle} alt="developer"  />
+                Sofi
+                <br />
+                web developer
+                </h1>
+                <h2>Frontend developer</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Home
