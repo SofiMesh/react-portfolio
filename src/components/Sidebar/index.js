ac7b50934c90920f3ab1026be050914c3af08e87
SofiMesh <sofimeshvildishvili@gmail.com>
2023-10-18 13:38:36 -0400

    getting ready for deployment

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faLinkedin,faGithub} from '@fortawesome/free-brands-svg-icons'
-import {faHome,faUser, faEnvelope, faClose, faSuitcase} from '@fortawesome/free-solid-svg-icons'
+import {faHome,faUser, faEnvelope, faSuitcase} from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
